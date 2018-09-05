0c61a9748eac6ac14d93515440b5f117e4e81761
Mary A <mary.alva@icloud.com>
2018-09-05 21:24:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return 
+    return "I can't hear you!"
+  } else if (string.toUpperCase() === string) {
+    return ""
   }
-} {else if (string.toUpperCase() === string)
 }
   
 }
