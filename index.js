03406ceba918264b0e703d3bf19c02312b303dee
Mary A <mary.alva@icloud.com>
2018-09-05 20:59:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+var string = "string"
+string.toLowerCase() === string
+function sayHiToGrandma(string) {
+    return ("I can't hear you!")
+}
+ 
+ 
+var string = "STRING"
+string.toUpperCase() === string
+function sayHiToGrandma(STRING) {
+    return ("YES INDEED!")
+}
+
+
+var string = "I love you, Grandma."
+function sayHiToGrandma(string) {
+    return ("I love you, too.")
+}
