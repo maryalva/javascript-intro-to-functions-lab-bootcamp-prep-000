203d672f23de6a4e44e38c9738412030f547c344
Mary A <mary.alva@icloud.com>
2018-09-05 21:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,23 +24,6 @@ function sayHiToGrandma(string) {
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else {
-    return  
+    return  "I love you, too."
+  }
 }
-  
-}
-  
-  
-  
-    return ("I can't hear you!")
-}
- 
-
-function sayHiToGrandma(STRING) {
-    return ("YES INDEED!")
-}
-
-function sayHiToGrandma(string) {
-    return ("I love you, too.")
-}
-
-
